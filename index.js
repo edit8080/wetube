803a36ae528e31fb1ef450a985cb61b0763e022c
edit8080 <tlfla01@gmail.com>
2021-03-12 13:24:58 +0900

    #2. Install Babel and nodemon

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-const express = require("express");
+import express from "express";
+//const express = require("express");
 const app = express();
 
 const PORT = 4000;
-// respond with "hello world" when a GET request is made to the homepage
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-}
-function handleHome(req, res) {
-  // 사용자 요청 (req)
-  console.log(req);
 
-  // 서버 응답(res) 전송
-  res.send("Hello from Home");
-}
-function handleProfile(req, res) {
-  res.send("You are on my profile");
-}
-// app.post("/",handleHome); - post 방식
+const handleHome = (req, res) => res.send("Hello from Home");
+const handleProfile = (req, res) => res.send("You are on my profile");
+
 app.get("/", handleHome);
-
-// "/profile" 경로로 접속 시 handleProfile 실행
 app.get("/profile", handleProfile);
 
 // 서버 포트 연결
