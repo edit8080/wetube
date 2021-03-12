c7281ff4d098852a4bd0ef5d53dc481a98d328c3
edit8080 <tlfla01@gmail.com>
2021-03-12 14:53:24 +0900

    #4. Routing

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { userRouter } from "./router";
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from Home");
 const handleProfile = (req, res) => res.send("You are on my profile");
@@ -19,11 +15,13 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
-app.use(morgan("common"));
+app.use(morgan("dev"));
 
 /* router */
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
-// 서버 포트 연결
-app.listen(PORT, handleListening);
+// import router
+app.use("/user", userRouter);
+
+export default app;
