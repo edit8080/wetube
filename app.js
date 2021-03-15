2b6b980d79829e84d957c238b2e90f836d546c7e
edit8080 <tlfla01@gmail.com>
2021-03-15 14:57:34 +0900

    #6. Creating Layout with Pug

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { localMiddleware } from "./middlewares";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -11,11 +12,14 @@ import routes from "./routes";
 const app = express();
 
 /* middleware */
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localMiddleware);
 
 /* router */
 app.use(routes.home, globalRouter);
