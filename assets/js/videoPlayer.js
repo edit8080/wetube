4b18ebb8fe150859314d28260857712417a8ffed
edit8080 <tlfla01@gmail.com>
2021-04-01 17:47:26 +0900

    #40. Mute and Unmute Video

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,16 +1,29 @@
 const videoContainer = document.getElementById("js-videoPlayer");
 const videoPlayer = document.querySelector("#js-videoPlayer video");
 const playBtn = document.getElementById("js-playButton");
+const volumeBtn = document.getElementById("js-volumeButton");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>';
   } else {
     videoPlayer.pause();
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
+  }
+}
+function handleVolumeClick() {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else {
+    videoPlayer.muted = true;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }
 }
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
+  volumeBtn.addEventListener("click", handleVolumeClick);
 }
 if (videoContainer) {
   init();
