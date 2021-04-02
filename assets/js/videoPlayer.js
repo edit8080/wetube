9d883cb9a6a43ebc60242da629e932de10b721e7
edit8080 <tlfla01@gmail.com>
2021-04-02 12:44:30 +0900

    #41. Enter and Exit FullScreen

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,6 +2,7 @@ const videoContainer = document.getElementById("js-videoPlayer");
 const videoPlayer = document.querySelector("#js-videoPlayer video");
 const playBtn = document.getElementById("js-playButton");
 const volumeBtn = document.getElementById("js-volumeButton");
+const fullScreenBtn = document.getElementById("js-fullScreenButton");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -21,9 +22,23 @@ function handleVolumeClick() {
     volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }
 }
+function exitFullScreen() {
+  document.exitFullscreen();
+  fullScreenBtn.innerHTML = '<i class="fas fa-expand"></i>';
+  fullScreenBtn.removeEventListener("click", exitFullScreen);
+  fullScreenBtn.addEventListener("click", goFullScreen);
+}
+function goFullScreen() {
+  videoContainer.webkitRequestFullscreen();
+  fullScreenBtn.innerHTML = '<i class="fas fa-compress"></i>';
+  fullScreenBtn.removeEventListener("click", goFullScreen);
+  fullScreenBtn.addEventListener("click", exitFullScreen);
+}
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
+  fullScreenBtn.addEventListener("click", goFullScreen);
+  document.addEventListener("fullscreenchange", exitFullscreen);
 }
 if (videoContainer) {
   init();
